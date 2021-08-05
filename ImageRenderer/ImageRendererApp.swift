387e9469aff08a6839bf46a38d67b02f4b8d303b
Shawn Koh <shawn@shawnkoh.sg>
2021-08-05 14:44:46 +0800

    Initial Commit

---
+++ ImageRenderer/ImageRendererApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageRendererApp.swift
+//  ImageRenderer
+//
+//  Created by Shawn Koh on 5/8/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageRendererApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
